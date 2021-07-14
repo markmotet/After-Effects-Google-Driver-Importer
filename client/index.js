41cafea15466ba7e67fea59a9faf4e18a9792d20
unknown <markmotet@gmail.com>
2021-07-14 15:50:28 -0700

    Thumbnails now appear after inputting a Google Drive folder ID and clicking the import button.

--- client/index.js
+++ client/index.js
@@ -1,8 +1,29 @@
-const folderId = 'FOLDER_ID';
-const apiKey = 'API_KEY';
-const folderUrl = 'https://www.googleapis.com/drive/v3/files?q=%' + folderId + '%27+in+parents&key=' + apiKey;
+const fs = require('fs');
+const https = require('https');
+const {google} = require('googleapis');
 
-function getFileId(theFolderUrl) {
+const CLIENT_ID = '';
+const CLIENT_SECRET = '';
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+
+const REFRESH_TOKEN = '';
+
+const oAuth2Client = new google.auth.OAuth2(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI
+);
+
+oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
+
+const drive = google.drive({
+    version: 'v3', 
+    auth: oAuth2Client
+});
+
+
+
+function getFileIds(theFolderUrl) {
   https.get(theFolderUrl, (response) => {
   
     var data = '';
@@ -12,51 +33,70 @@ function getFileId(theFolderUrl) {
     });
   
     response.on('end', () => {
-      var theId = JSON.parse(data).files[1].id;
-      someFunc(theId)
-      return theId;
+
+      var files = JSON.parse(data).files;
+      var ids = [];
+
+      for (var i = 0; i < files.length; i++) {
+        ids[i] = files[i].id;
+      }
+
+      getDataFromFileId(ids);
     });
-  
   })
 }
 
-async function someFunc(id) {
-  const reponse = await drive.files.get({
-    fileId: id,
-    fields: '*'
-  });
-}
-//getFileId(folderUrl);
+async function getDataFromFileId(ids) {
+
+  for(var i = 0; i < ids.length; i++) {
+
+      const response = await drive.files.get({
+        fileId: ids[i],
+        fields: '*' // Only add necessary ones
+      });
+
+      console.log(response.data.thumbnailLink);
+
+      addImage(response.data.thumbnailLink);
 
 
+  }
 
-
-function toggleOptions() {
-
-  const options = document.getElementById("options"); 
-
-  if (options.style.display === "none") {
-    options.style.display = "block";
-  } 
-  else {
-    options.style.display = "none";
-  } 
 }
 
 
-   
 
 
+function addImage(imageUrl) {
+
+  const gridContainer = document.getElementById('grid-container');
+
+  const newGridElement = document.createElement('div');
+  newGridElement.className = 'grid-item';
+
+  const newGridImage = document.createElement('img');
+  newGridImage.src = imageUrl;
+
+  newGridElement.appendChild(newGridImage);
+  gridContainer.appendChild(newGridElement);
+
+}
 
 
+/* 1) Create an instance of CSInterface. */
+var csInterface = new CSInterface();
+/* 2) Make a reference to your HTML button and add a click handler. */
+var importButton = document.getElementById('import-button');
+
+importButton.addEventListener('click', () => {
+  alert(document.getElementById('url-input').value);
+  
+  const folderId = document.getElementById('url-input').value; //271MuAMCgwtfi5m9MGXXsQDBEn3AR6R7uED //Doesn't work with other folders?
+  const apiKey = 'AIzaSyDsrn8JHxDN7wKLMDborhElW4hC65RnSLE';
+  const folderUrl = 'https://www.googleapis.com/drive/v3/files?q=%27' + folderId + '%27+in+parents&key=' + apiKey;
+
+  getFileIds(folderUrl);
 
 
-
-// /* 1) Create an instance of CSInterface. */
-// var csInterface = new CSInterface();
-// /* 2) Make a reference to your HTML button and add a click handler. */
-// var applyButton = document.querySelector("#apply-button");
-// applyButton.addEventListener("click", () => {
-//   downloadFile('1rWYM2BlCVDQuujlaTdBEh45ekRJwHTwe', 'me.jpg');
-// });
+});
 
